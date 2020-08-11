8fc8a18a80afb1a8fb9ca11155ea0b2ee6e867f6
Yohann MARTIN <mimichackeur@gmail.com>
2020-08-11 22:35:24 +0200

    Fixed the genre filter

--- webapi/nekosama/resources/anime.py
+++ webapi/nekosama/resources/anime.py
@@ -1,7 +1,7 @@
 import re
 from flask import request
 from flask_restful import Resource
-from sqlalchemy import or_
+from sqlalchemy import or_, all_
 
 from ..models.anime import Anime, AnimeGenre, animes_schema
 
@@ -19,7 +19,7 @@ class AnimeListResource(Resource):
         type = data.get('type', None)
         status = data.get('status', None)
         year = data.get('year', None)
-        genres = data.get('genres', None)
+        genres = data.getlist('genres', None)
         sort = data.get('sort', 'score_desc')
         sort_by = '_'.join(sort.split('_')[0:-1])
         sort_dir = sort.split('_')[-1]
@@ -44,11 +44,12 @@ class AnimeListResource(Resource):
             animes = animes.filter(Anime.start_date_year == int(year))
 
         if genres is not None:
-            if not isinstance(genres, list):
-                genres = [genres]
-            genres = AnimeGenre.query.filter(AnimeGenre.name.in_(genres))
+            print(genres)
+            # genres = AnimeGenre.query.filter(AnimeGenre.name.in_(genres))
+            animes = animes.join(Anime.genres)
             for genre in genres:
-                animes = animes.filter(Anime.genres.in_(genre))
+                animes = animes.filter(Anime.genres.any(AnimeGenre.name == genre))
+            # animes = animes.filter(Anime.genres.in_(genre))
 
         animes = animes.order_by(getattr(getattr(Anime, sort_by), sort_dir)())
         animes = animes.paginate(page, perPage, error_out=False)
