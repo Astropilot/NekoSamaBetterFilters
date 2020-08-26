8c93d6b245e3356a08767d6c042861e512b7ad05
Yohann <ymartin@weyield.io>
2020-08-26 20:46:23 +0200

    Improved the options page

--- webextension/source/options/options.ts
+++ webextension/source/options/options.ts
@@ -1,4 +1,4 @@
-import {optionsStorage} from './options-storage';
+import {optionsStorage} from '../options-storage';
 
 const perms = [
   '*://*.inpagepush.com/*',
--- webextension/webpack.config.ts
+++ webextension/webpack.config.ts
@@ -23,7 +23,7 @@ const config: Configuration = {
     'hijacking/pstream/pstream-requests',
     'hijacking/hijack-js',
     'hijacking/hijack-requests',
-    'options'
+    'options/options'
   ].map(name => [name, `./source/${name}`])),
   output: {
     path: path.join(__dirname, 'distribution'),
